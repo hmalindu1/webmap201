d3198556f8a7b765a084ec55b23c966826b30d80
Hashan <hemachandra.hashan@gmail.com>
2021-06-15 11:50:15 +0530

    format on save done by "PHP fotmatter" extension

--- load_gnd.php
+++ load_gnd.php
@@ -1,18 +1,17 @@
-<?php 
+<?php
 $dsn = "pgsql:host=localhost;dbname=webmap201;port=5432";
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false
-        ];
-        $pdo = new PDO($dsn, 'postgres', '123', $opt);
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, 'postgres', '123', $opt);
 
-        $result = $pdo -> query("SELECT *, ST_AsGeoJSON(geom,5) AS geojson FROM gnd");
-        foreach ($result AS $row){
-            unset($row['geom']);
+$result = $pdo->query("SELECT *, ST_AsGeoJSON(geom,5) AS geojson FROM gnd");
+foreach ($result as $row) {
+    unset($row['geom']);
     $geometry = $row['geojson'] = json_decode($row['geojson']);
     unset($row['geojson']);
     $feature = ["type" => "features", "geometry" => $geometry, "properties" => $row];
     echo json_encode($feature), "<br><br>";
 }
-?>
